0c2f114bf386f366f66590734cb7f820ace45e73
Velid Duranovic <velid.duranovic@endava.com>
2023-09-15 18:14:28 +0200

    Added new type (User), new service for getting data from json file, and added table in App.tsx and displaying data.

---
+++ src/services/userService.tsx
@@ -0,0 +1,14 @@
+export const getData = async () => {
+    try {
+      const response = await fetch('data.json', {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+      });
+      return await response.json();
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  };
